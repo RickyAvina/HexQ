d04d7b1d5e9d2d9d0097f67617669f312b431bde
Enrique Aviña <rickyavina@icloud.com>
2021-01-06 14:23:59 -0800

    1. Seperated MDP into own file\n2. Started construction of higher level MDPs with the following structure: One MDP for each MER, actions which are exits of those MERs

--- hexq/hexQ.py
+++ hexq/hexQ.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+from hexq.mdp import MDP
 
 
 class HexQ:
@@ -14,11 +15,12 @@ class HexQ:
         self._init_mdps()
 
     def _init_mdps(self):
-        for i in range(self.state_dim):
-            self.mdps[i] = MDP(self.target)
+        MDP.env = self.env
+        root_mdp = MDP(0, None) 
 
         # initialize base mdp with primitive actions
-        self.mdps[0].set_actions((0, 1, 2, 3))
+        root_mdp.actions = {0: 0, 1: 1, 2: 2, 3: 3}
+        self.mdps[0] = root_mdp
 
     def find_freq(self):
         s = self.env.reset()
@@ -40,133 +42,140 @@ class HexQ:
         return sorted_order
 
     def alg(self):
-        #freq = self.find_freq()
-
         import sys
-        for e in range(self.state_dim):
-            transition_probs, exits, entries = self.explore(self.mdps[e], e)
-            mers = self.find_MERs(self.mdps[e])
-            # from MERS, create sub-mpds
-            # time to create sub-mdps, that means we create an MDP
-            sub_mdps = self.create_sub_MDPs(self.mdps[e], mers)
-            sys.exit()
 
-    def explore(self, mdp, e):
+        #freq = self.find_freq()
+        # level zero (primitive actions)
+        mdp = self.mdps[0]
+        transition_probs, exits, entries = self.explore(mdp=mdp, level=0)
+
+        mers = self.find_MERs(mdp)
+
+        # from MERS, create sub-mpds
+        sub_mdps = self.create_sub_MDPs(mdp, mers, 1)
+        
+
+        # train each sub-mdp, should yield a policy to reach each resp exit
+        # an additional sub-mdp should be trained to navigate from exits to goal
+
+        # create abstract actions and initation set for each sub-mdp
+        # this means that each sub-mdp should be numbered according to their room
+
+        # find value function for s, a pairs in sub_mdp
+        # Actions of next level = exits of current level
+        '''
+        next_actions = dict()
+            index = 0
+            for sub_mdp in sub_mdps:
+                next_actions[index] = sub_mdp.target
+
+            sys.exit()
+        '''
+
+    def train_sub_mdps(self, mdp, sub_mdps):
+        pass
+
+    def explore(self, mdp, level):
         s = self.env.reset()
 
         for _ in range(self.exploration_steps):
             # select actions from action set
             a = mdp.select_random_action()
-            s_p, r, d, _ = self.env.step(action=a)
-            mdp.add_trans(s, a, s_p)
+            if level == 0:
+                s_p, r, d, _ = self.primitive_trans(mdp, s, a)
+            else:
+                # multiple steps
+                s_p, r, d, _ = self.multistep_trans(mdp, s, a)
+
             s = s_p
 
         trans = mdp._count_to_probs()
         return trans, mdp.exits, mdp.entries
 
-    def find_MERs(self, mdp):
-        ''' full BFS to find SCCs '''
+    def primitive_trans(self, mdp, s, a):
+        s_p, r, d, _ = self.env.step(action=a)
 
-        states = mdp.states.copy()
+        # add to entire state space
+        MDP.states.add(s)
+        mdp.mer.add(s)
+
+        # fill in root adj to find MERs
+        if s not in mdp.adj:
+            mdp.adj[s] = set()
+        mdp.adj[s].add(s_p)
+
+        # fill in transition probs
+        if (s, a) not in mdp.trans_count:
+            mdp.trans_count[(s, a)] = {s_p: 1}
+        elif s_p not in mdp.trans_count[(s, a)]:
+            mdp.trans_count[(s, a)][s_p] = 1
+        else:
+            mdp.trans_count[(s, a)][s_p] += 1
+
+        # fill in exit/entries
+        if s[1:] != s_p[1:]:
+            mdp.exit_pairs.add((s, s_p))
+            mdp.exits.add((s, a))
+            mdp.entries.add(s_p)
+
+        return s_p, r, d, None
+
+    def multistep_trans(self, mdp, s, a):
+        # multiple steps
+        # This MDP should select random acttions which are policies to exits of
+        # previous level. There should be an initation set (states where you can
+        # execute a certain policy/pick actions from.
+        # An action should take you to the exit, and then you take the exit action
+
+        # sample random action from actions
+        pass
+
+    def find_MERs(self, mdp):
+        ''' MERs are just states with deterministic intra-region transitions '''
+
+        states = mdp.mer.copy()  # for level 0, this is all the states
         mers = []
 
         while len(states) > 0:
             s = random.choice(tuple(states))
-            mer = {s}
-            self.bfs(mdp, states, s, mer)
+            mer = self.bfs(mdp, states, s)
             mers.append(mer)
-
         return mers
 
-    def create_sub_MDPs(self, mdp, mers):
-        # for every MER, create sub MDP
-        # Pair exits with MERs
-        exit_states = {state[0] for state in mdp.exits}
-        sub_mdps = []
-        for mer in mers:
-            # create a sub-mdp for every exit
-            for state in exit_states:
-                if state in mer:
-                    sub_mdp = MDP(state)  # exit is the target
-                    sub_mdp.states = mer
-                    sub_mdps.append(sub_mdp)
-        return sub_mdps
+    def bfs(self, mdp, states, s, mer=None):
+        if mer is None:
+            mer = set()
 
-    def bfs(self, mdp, states, s, mer):
         if s in states:
             states.remove(s)
         mer.add(s)
 
         for neighbor in mdp.adj[s]:
-            if neighbor in states and (s, neighbor) not in mdp.exits:
+            if neighbor in states and (s, neighbor) not in mdp.exit_pairs:
                 self.bfs(mdp=mdp, states=states, s=neighbor, mer=mer)
 
-class MDP:
-    '''
-    states are a set
-    actions are a tuple
-    '''
+        return mer
 
-    def __init__(self, target=None):
-        self.states = set()
-        self.actions = ()
-        self.trans_count = {}  # (s, a) -> {s_p: count, s_p': count'}
-        self.adj = {}
-        self.trans_probs = None
-        self.target = target
-        self.exits = set()  # {(s, s'), ...}
-        self.entries = set()  # {s', ...}
-   
-    def __str__(self):
-        return self.__rep__()
+    def create_sub_MDPs(self, mdp, mers, level):
+        # Three architectures:
+        # 1) One MDP per level, initiation set and different policies per exit
+        # 2) One MDP per MER/exit pair. This is the one used in the HEXQ alg.
+        # 3) One MDP per MER (with various exits), each action will be a diff exit policy
+        sub_mdps = []
 
-    def __rep__(self):
-        return "target: {} actions: {}\nexits: {}".format(self.target,
-                self.actions, self.exits)
+        # Picking arch 3
+        for mer in mers:
+            random_state = random.choice(tuple(mer))
+            sub_mdp = MDP(level=level, state_var=random_state[level])
+            sub_mdp.mer = mer
+            count = 0
 
-    def add_state(self, state):
-        self.states.add(state)
+            # find exits that correspond to this MER
+            for state, action in mdp.exits:
+                if state in mer:
+                    sub_mdp.exits.add((state, action))
+                    sub_mdp.actions[count] = state
+                    count += 1
 
-    def set_actions(self, actions):
-        self.actions = actions
-
-    def add_action(self, action):
-        self.actions = self.actions + (action, )
-
-    def select_random_action(self):
-        return random.choice(self.actions)
-
-    def add_trans(self, s, a, s_p):
-        # record states
-        self.states.add(s)
-
-        # fill in adj dictionary
-        if s not in self.adj:
-            self.adj[s] = set()
-        self.adj[s].add(s_p)
-
-        # fill in transition probabilities and entries/exits
-        if s != self.target:
-            if (s, a) not in self.trans_count:
-                self.trans_count[(s, a)] = {s_p: 1}
-            elif s_p not in self.trans_count[(s, a)]:
-                self.trans_count[(s, a)][s_p] = 1
-            else:
-                self.trans_count[(s, a)][s_p] += 1
-
-            if s[1] != s_p[1]:  # exit/entry
-                self.exits.add((s, s_p))
-                self.entries.add(s_p)
-
-    def _count_to_probs(self):
-        trans_probs = {}
-
-        for s_a in self.trans_count:
-            if s_a not in trans_probs:
-                trans_probs[s_a] = {}
-
-            total_count = sum(self.trans_count[s_a].values())
-            for s_p in self.trans_count[s_a]:
-                trans_probs[s_a][s_p] = self.trans_count[s_a][s_p] / total_count
-
-        return trans_probs
+            sub_mdps.append(mdp)
+        return sub_mdps
---
+++ hexq/mdp.py
@@ -0,0 +1,97 @@
+import random
+
+
+class MDP:
+    '''
+    states are a set
+    actions are a tuple
+    '''
+
+    # states are shared across all MDPs
+    states = set()
+    env = None
+
+    def __init__(self, level, state_var):
+        self.state_var = state_var
+        self.level = level
+
+        self.mer = set()
+        self.actions = {}   # R => exits (for primitives, key=value)
+
+        self.trans_count = {}  # (s, a) -> {s_p: count, s_p': count'}
+        # In future, could be a frozen dict {s: a: {s_p: count, s_p': count'}, a': {}}
+        self.adj = {}
+        self.trans_probs = None
+
+        self.exit_pairs = set()  # {(s, s_p), ...}
+        self.exits = set()  # {(s, a), ...}
+        self.entries = set()  # {s', ...}
+
+    def __str__(self):
+        return self.__rep__()
+
+    def __rep__(self):
+        return "(MDP) level {} var {} actions {}".format(self.level, self.state_var, self.actions)
+
+    def select_random_action(self):
+        return random.choice(self.actions)
+
+    def add_trans(self, s, a, s_p):
+        # transitions are deterministic after primitive level
+        # states are also known, however, it is kept for convenience
+
+        # record states
+        self.states.add(s)
+
+        # fill in adj dictionary
+        if s not in self.adj:
+            self.adj[s] = set()
+        self.adj[s].add(s_p)
+
+        # fill in transition probabilities and entries/exits
+        if s != self.target:
+            if (s, a) not in self.trans_count:
+                self.trans_count[(s, a)] = {s_p: 1}
+            elif s_p not in self.trans_count[(s, a)]:
+                self.trans_count[(s, a)][s_p] = 1
+            else:
+                self.trans_count[(s, a)][s_p] += 1
+
+            if s[1] != s_p[1]:  # exit/entry
+                self.exit_pairs.add((s, s_p))
+                self.exits.add((s, a))
+                self.entries.add(s_p)
+
+    def _count_to_probs(self):
+        trans_probs = {}
+
+        for s_a in self.trans_count:
+            if s_a not in trans_probs:
+                trans_probs[s_a] = {}
+
+            total_count = sum(self.trans_count[s_a].values())
+            for s_p in self.trans_count[s_a]:
+                trans_probs[s_a][s_p] = self.trans_count[s_a][s_p] / total_count
+
+        return trans_probs
+
+    def find_MERs(self):
+        ''' MERs are just states with deterministic intra-region transitions '''
+        states = self.states.copy()
+        mers = []
+
+        while len(states) > 0:
+            s = random.choice(tuple(states))
+            mer = {s}
+            self.bfs(states, s, mer)
+            mers.append(mer)
+
+        return mers
+
+    def bfs(self, states, s, mer):
+        if s in states:
+            states.remove(s)
+            mer.add(s)
+            for neighbor in self.adj[s]:
+                if neighbor in states and (s, neighbor) not in self.exit_pairs:
+                    self.bfs(states=states, s=neighbor, mer=mer)
